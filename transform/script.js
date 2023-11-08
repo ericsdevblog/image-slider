fd33a614e41b525e8d240e2f3912e423f2e3fbf4
Eric Hu <huericnan@gmail.com>
2023-11-08 15:16:45 -0500

    fix showSlide

--- transform/script.js
+++ transform/script.js
@@ -2,24 +2,20 @@ const slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
 
 function showSlide(index) {
-    slides.forEach((slide, i) => {
-        const slideWidth = slide.clientWidth;
-        if (i === index) {
-            slide.style.transform = `translateX(-${index * slideWidth}px)`;
-        } else {
-            slide.style.transform = `translateX(-${index * slideWidth}px)`;
-        }
-    });
+  slides.forEach((slide, i) => {
+    const slideWidth = slide.clientWidth;
+    slide.style.transform = `translateX(-${index * slideWidth}px)`;
+  });
 }
 
 function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
 }
 
 function prevSlide() {
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-    showSlide(currentSlide);
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
 }
 
 showSlide(currentSlide);
