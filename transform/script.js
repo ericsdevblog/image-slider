3649e61d2adf499a0f3902af7d32a460366cce55
Eric Hu <huericnan@gmail.com>
2023-11-08 11:46:42 -0500

    update

--- transform/script.js
+++ transform/script.js
@@ -1,25 +1,13 @@
 const slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
 
-// function showSlide(index) {
-//     slides.forEach((slide, i) => {
-//         if (i === index) {
-//             slide.style.opacity = 100;
-//         } else {
-//             slide.style.opacity = 0;
-//         }
-//     });
-// }
-
 function showSlide(index) {
     slides.forEach((slide, i) => {
         const slideWidth = slide.clientWidth;
         if (i === index) {
             slide.style.transform = `translateX(-${index * slideWidth}px)`;
-            // slide.style.opacity = 100;
         } else {
             slide.style.transform = `translateX(-${index * slideWidth}px)`;
-            // slide.style.opacity = 0;
         }
     });
 }
